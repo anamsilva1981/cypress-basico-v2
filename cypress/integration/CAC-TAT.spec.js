939b148c9ed11553d53bf131fa110a053e2dc270
ana-msilva <ana.msilva@ibm.com>
2024-02-21 17:34:36 -0300

    feat: teste campos obrigatorios do formulario

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,19 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
-        
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-    })
+describe("Central de Atendimento ao Cliente TAT", function () {
+  beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
 
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
 
-  })
+  it.only("preenche os campos obrigatórios e envia o formulario", function () {
+    cy.get("#firstName").type('Ana');
+    cy.get("#lastName").type('Silva');
+    cy.get("#email").type('email@email.com');
+    cy.get('#open-text-area').type('Texto');
+    cy.get('button[type="submit"]').click();
+  });
+});
